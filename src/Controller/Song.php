76f9e5343e67dc60bdf80d6fb83621b8b53f87c6
Torleif Berger <torleif@outlook.com>
2013-04-06 22:28:11 +0200

    Removed unused Controller_Song::get_xhr

--- src/Controller/Song.php
+++ src/Controller/Song.php
@@ -6,17 +6,4 @@ class Controller_Song
 	{
 		echo new View_Song($id, $key);
 	}
-
-	function get_xhr($id, $key = NULL)
-	{
-		$song = Model_Song::get($id);
-		$html = Geekality\Transposer::parse($song->text, $song->key);
-
-		if($key !== NULL)
-			$html->transpose($key);
-		else
-			$key = $song->key;
-
-		echo json_encode(array('key' => $key, 'html' => ''.$html));
-	}
 }
