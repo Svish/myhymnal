983d8bd74a51326d045603f1288df97c9a4c0e7c
Torleif Berger <torleif@outlook.com>
2013-04-11 22:08:53 +0200

    Added subtitle to header title

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ return array
 	'site' => array
 	(
 		'title' => 'My Hymnal',
+		'subtitle' => 'Lyrics and chords. Clean and correct.',
 		'base' => WEBROOT,
 		'base_abs' => 'http://'.$_SERVER['HTTP_HOST'].WEBROOT,
 		'isProduction' => $_SERVER['SITE_ENV'] == 'prod',
