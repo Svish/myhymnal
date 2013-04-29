6157c2f6bd0ab28ea62ff24176d5b20a38d91a2b
Torleif Berger <torleif@outlook.com>
2013-04-30 01:42:56 +0200

    Added HTTP::head

--- src/HTTP.php
+++ src/HTTP.php
@@ -2,6 +2,36 @@
 
 class HTTP
 {
+	public static function head($url)
+	{
+		Timer::start(__METHOD__, func_get_args());
+
+		$request = curl_init();
+		curl_setopt_array($request, array
+		(
+		     CURLOPT_URL => $url,
+		     
+		     CURLOPT_RETURNTRANSFER => TRUE,
+		     CURLOPT_HEADER => TRUE,
+		     CURLOPT_NOBODY => TRUE,
+
+		     CURLOPT_FOLLOWLOCATION => TRUE,
+		     CURLOPT_MAXREDIRS => 10,
+		));
+		$response = curl_exec($request);
+
+		if($response === FALSE)
+			$response = new Exception(curl_error($request), curl_errno($request));
+
+		curl_close($request);
+		Timer::stop();
+
+		if($response instanceof Exception)
+			throw $response;
+
+		return $response;
+	}
+
 	public static function get($url)
 	{
 		Timer::start(__METHOD__, func_get_args());
@@ -18,9 +48,15 @@ class HTTP
 		     CURLOPT_MAXREDIRS => 10,
 		));
 		$response = curl_exec($request);
-		curl_close($request);
 
+		if($response === FALSE)
+			$response = new Exception(curl_error($request), curl_errno($request));
+
+		curl_close($request);
 		Timer::stop();
+
+		if($response instanceof Exception)
+			throw $response;
 
 		return $response;
 	}
