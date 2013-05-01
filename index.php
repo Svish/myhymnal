161de044299f1e15c513fe658b8f0985f6684b55
Torleif Berger <torleif@outlook.com>
2013-05-01 14:19:56 +0200

    Cleanup $_GET after getting path

--- index.php
+++ index.php
@@ -19,14 +19,17 @@ setlocale(LC_ALL, 'en_US.utf-8', 'eng');
 require 'vendor/autoload.php';
 
 
+
+// Get path
 $uri = isset($_GET['path_uri'])
     ? $_GET['path_uri']
     : NULL;
-Timer::start('Request', array($uri));
+unset($_GET['path_uri']);
 
+// Handle request
+Timer::start('Request', array($uri));
 View::engine()->setHelpers(include CONFROOT.'mustache_helpers.php');
 Website::init(include CONFROOT.'routes.php', 'Controller_Error')
     ->serve($uri);
-
 Cache::delete('rid', NULL, 30*60);
 Cache::set('rid', RID, Timer::result());
