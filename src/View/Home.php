e01ec0ea7090ae19efb732ffcdd70f112072cc7b
Torleif Berger <torleif@outlook.com>
2013-06-20 16:21:36 +0200

    Latest on home page instead of all songs

--- src/View/Home.php
+++ src/View/Home.php
@@ -5,9 +5,8 @@ class View_Home extends View
 	public function __construct()
 	{
 		Timer::start(__METHOD__);
-		$this->songs = View::factory('SongIndex')->render(FALSE);
+		$this->songs = Model_Song::list_last_updated();
 		$this->top = View::factory('TopPages')->render(FALSE);
-		//$this->books = View::factory('BookIndex')->render(FALSE);
 		Timer::stop();
 	}
 }
